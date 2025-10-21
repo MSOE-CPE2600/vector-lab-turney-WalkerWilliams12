5427ae4a7e873f643da8dfe13b59a098b36702ae
Walker Williams <williamsw@msoe.edu>
2025-10-21 10:53:30 -0500

    vector_math.c completed and commented

--- vector_math.c
+++ vector_math.c
@@ -10,17 +10,38 @@
  */
 #include "vector.h"
 
-Vector add(Vector a, Vector b) {
+/**
+ * @brief Adds 2 vectors together
+ * @param a - first vector to add to
+ * @param b - second vector to add magnitudes to vector a
+ * @return the resulting vector from adding a and b
+ */
+Vector add(Vector a, Vector b) 
+{
     Vector r = {"", a.x + b.x, a.y + b.y, a.z + b.z, 1};
     return r;
 }
 
-Vector subtract(Vector a, Vector b) {
+/**
+ * @brief Subtracts one vector from another
+ * @param a - the initial vector to be subtracted from
+ * @param b - the vector values to subtract from a
+ * @return the resulting vector from subtracting b from a
+ */
+Vector subtract(Vector a, Vector b) 
+{
     Vector r = {"", a.x - b.x, a.y - b.y, a.z - b.z, 1};
     return r;
 }
 
-Vector scalar_multiply(Vector v, double scalar) {
+/**
+ * @brief Multiplies a vector by a scalar value
+ * @param v - vector to multiply
+ * @param scalar - value to multiply v by
+ * @return the resulting vector from multiplying v by the scalar
+ */
+Vector scalar_multiply(Vector v, double scalar) 
+{
     Vector r = {"", v.x * scalar, v.y * scalar, v.z * scalar, 1};
     return r;
 }
