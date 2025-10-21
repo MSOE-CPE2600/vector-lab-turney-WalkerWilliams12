eda33bbe71507d9231d2c3b6b93aab4ec0c81956
Walker Williams <williamsw@msoe.edu>
2025-10-21 10:48:00 -0500

    vector_data.c updated, completed and commented

--- vector_data.c
+++ vector_data.c
@@ -7,24 +7,39 @@
  * Assignment: Lab 5
  * Name: Walker Williams
  * 
+ * //Data is not storing more than one variable at a time right now.
  */
 #include "vector.h"
 
+#define MAX_VECTORS 10
+
 static Vector vectors[MAX_VECTORS];
 
+/**
+ * @brief Clears vector from the array
+ */
 void clear_vectors() {
     for (int i = 0; i < MAX_VECTORS; i++)
         vectors[i].active = 0;
     printf("All vectors cleared.\n");
 }
 
+/**
+* @brief Adds vectors into the array
+* @param v - vector to add to the array
+* @return 1 if the vector is added into the array
+* @return 0 if the array is full and vector not added
+*/
 int add_vector(Vector v) {
+    // Replace existing vector if same name found
     for (int i = 0; i < MAX_VECTORS; i++) {
-        if (vectors[i].active && strcmp(vectors[i].name, vectors[i].name) == 0) {
+        if (vectors[i].active && strcmp(vectors[i].name, v.name) == 0) {
             vectors[i] = v;
             return 1;
         }
     }
+
+    // find first empty slot
     for (int i = 0; i < MAX_VECTORS; i++) {
         if (!vectors[i].active) {
             vectors[i] = v;
@@ -32,10 +47,18 @@ int add_vector(Vector v) {
             return 1;
         }
     }
+
+    // if ther is no more free slots in array
     printf("Memory full. Cannot store new vector.\n");
     return 0;
 }
 
+/**
+* @brief Finds if a vector is in the array
+* @param name the name of the vector being serched for
+* @return the address of the vector if it exists
+* @return null if the vector does not exist
+*/
 Vector *find_vector(const char *name) {
     for (int i = 0; i < MAX_VECTORS; i++) {
         if (vectors[i].active && strcmp(vectors[i].name, name) == 0)
@@ -44,6 +67,9 @@ Vector *find_vector(const char *name) {
     return NULL;
 }
 
+/**
+* @brief Lists all of the vectors currently saved in the array
+*/
 void list_vectors() {
     printf("Stored Vectors:\n");
     for (int i = 0; i < MAX_VECTORS; i++) {
