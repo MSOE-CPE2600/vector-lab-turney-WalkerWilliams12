4117aad19156babb6d892dc9c3bb18b572dc0f55
Walker Williams <williamsw@msoe.edu>
2025-10-21 10:45:45 -0500

    vector.h completed and commented

--- vector.h
+++ vector.h
@@ -18,25 +18,29 @@
 
 #define MAX_VECTORS 10
 
+/**
+ * @brief - creates a vector with a x, y, and z variable values
+ * @property x - x magnitude of the vector
+ * @property y - y magnitude of the vector
+ * @property z - z magnitude of the vector
+ * @property active - if the vector is being saved or deleted out of the array
+ */
 typedef struct {
     char name[20];
     double x, y, z;
     int active;
 } Vector;
 
-// ===== vector_math.c =====
 Vector add(Vector a, Vector b);
 Vector subtract(Vector a, Vector b);
 Vector scalar_multiply(Vector v, double scalar);
 
-// ===== vector_store.c =====
 void clear_vectors();
 int add_vector(Vector v);
 Vector *find_vector(const char *name);
 void list_vectors();
 
-// ===== vector_ui.c =====
-void run_ui();
+void vector_program();
 void display_help();
 
 #endif
