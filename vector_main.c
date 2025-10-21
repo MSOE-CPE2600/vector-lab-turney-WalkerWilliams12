7756950b2a5a005f8b9ef3a3093ed86bb6c56976
Walker Williams <williamsw@msoe.edu>
2025-10-21 10:55:24 -0500

    vector_main.c completed and commented

--- vector_main.c
+++ vector_main.c
@@ -7,18 +7,30 @@
  * Assignment: Lab 5
  * Name: Walker Williams
  * 
+ * Algorithm
+ * -gets base of vector program form vector_info
+ * -vector_info uses both vector_data and vector_math to handle vectors
+ * -vector_math has all functions to operate with the vectors
+ * -vector_data has all functions to handle the vector array
  * 
  * Compile: gcc -o minimat vector_main.c vector_math.c vector_data.c vector_info.c
  */
 #include "vector.h"
 
+/**
+* @brief Runs vector calculator
+* @param argc - not used
+* @param argv - "-h" to print command list
+* @return 0 to finish program
+*/
 //Main to run the entire program
 int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "-h") == 0) {
+    if (argc > 1 && strcmp(argv[1], "-h") == 0) 
+    {
         display_help();
         return 0;
     }
 
-    run_ui();
+    vector_program();
     return 0;
 }
